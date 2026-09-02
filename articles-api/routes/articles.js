ee658f84bf6cedc71196de1cf4e00307f24a5aed
maryam_mahmoudi <maryammhm9622@gmail.com>
2026-09-03 03:30:06 +0430

    Add articles pagination

--- articles-api/routes/articles.js
+++ articles-api/routes/articles.js
@@ -6,14 +6,44 @@ const AppError = require("../utils/AppError")
 
 const router = express.Router()
 
-router.get("/" , async (req , res) => {
+router.get("/" , async (req , res , next) => {
     try{
+        const page = Number(req.query.page)
+        if(req.query.page && (!Number.isInteger(page) || page < 1)){
+            throw new AppError("Invalid page" , 400)
+        }
+        const currentPage = page || 1
+
+        const limit = Number(req.query.limit)
+
+        if(req.query.limit && (!Number.isInteger(limit) || limit < 1)){
+            throw new AppError("Invalid limit" , 400)
+        }
+        const currentLimit = limit || 5
+
+
+        const total = await Article.countDocuments()
+        const totalPages = Math.ceil(total / currentLimit)
+        if ( currentPage > totalPages && total > 0 ){
+            throw new AppError("Page not found" , 404)
+        }
+        
         const articles = await Article.find()
-        res.status(200).json(articles)
-    } catch(error) {
-        res.status(500).json({
-            message : "Failed to get articles"
+                               .skip((currentPage - 1) * limit)
+                               .limit(currentLimit)
+
+        console.log(total)
+        console.log(totalPages)
+
+        res.status(200).json({
+            currentPage,
+            currentLimit,
+            total,
+            totalPages,
+            articles
         })
+    } catch(error) {
+        next(error)
     }
 })
 
@@ -62,11 +92,7 @@ router.put("/:id" , async (req , res , next) => {
     try{
         const result = check(req.body)
         if(result !== true){
-            return res.status(400).json({
-                message : "Validation Failed",
-                errors: result
-            })
-
+            throw new AppError("Validation Failed" , 400 , result)
         }
 
         const article = await Article.findByIdAndUpdate(
